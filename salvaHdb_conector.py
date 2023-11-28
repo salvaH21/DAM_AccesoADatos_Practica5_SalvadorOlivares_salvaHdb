206b033ef4913ed4d850021d85be7bba4ce9e0f1
salvaH21 <sholivares90@gmail.com>
2023-11-28 17:07:03 +0100

    insert_text implementado en la clase

--- salvaHdb_conector.py
+++ salvaHdb_conector.py
@@ -15,3 +15,22 @@ class SalvaHdb:
             return("OK")
         else:
             return("KO")
+
+    def insert_text(self,coleccion,documento,contenido):
+        self.operacion = "insert_text"
+        self.coleccion = coleccion
+        self.documento = documento
+        self.contenido = contenido
+        comando = '"C:\\Users\\salva\\Documents\\GitHub2\\salvaHdb\\salvaHdb.exe" '+self.operacion+' '+self.basededatos+' '+self.coleccion+' '+self.documento+' "'+self.contenido+'"'
+        resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+
+        if resultado.returncode == 0:
+            return("OK")
+        else:
+            return("KO")
+
+##insert_text
+##remove_file
+##create_collection
+##remove_collection
+##select
