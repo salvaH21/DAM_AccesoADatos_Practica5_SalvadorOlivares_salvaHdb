d6d4b629e45065745ea5baf063228a612a81ad8a
salvaH21 <sholivares90@gmail.com>
2023-11-28 13:31:10 +0100

    primera ejecución

--- Conexión.py
+++ Conexión.py
@@ -1,3 +1,12 @@
 ##Práctica 5:
 ##Creación de un conector a nuestra base de datos usando Python como lenguaje.
 ##Ampliar las funcionalidades del conector con respecto a lo que hago en la práctica.
+import subprocess
+
+comando = '"C:\\Users\\salva\\Documents\\GitHub2\\salvaHdb\\salvaHdb.exe" insert_file pruebas libros aventuras "la isla del tesoro"'
+resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+
+if resultado.returncode == 0:
+    print("OK")
+else:
+    print("KO")
