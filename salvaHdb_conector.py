614e7291e9ff9f56c87f88b543fe525df0e2d406
salvaH21 <sholivares90@gmail.com>
2023-11-29 11:08:26 +0100

    select funcionando implementado!

--- salvaHdb_conector.py
+++ salvaHdb_conector.py
@@ -70,13 +70,9 @@ class SalvaHdb:
         self.documento = documento
         comando = '"C:\\Users\\salva\\Documents\\GitHub2\\salvaHdb\\salvaHdb.exe" '+self.operacion+' '+self.basededatos+' '+self.coleccion+' '+self.documento+''
         resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-
+        
         if resultado.returncode == 0:
             print("OK")
-            archivo = open((documento+".json"),"r")
-            print(archivo.read())
+            print(resultado.stdout)
         else:
             return("KO")
-
-
-##select
